5b496364d96d86b568c2a48343c5fa7ae5d15e7c
JohnNWarila <johnwarila@gmail.com>
2019-02-17 20:07:59 -0800

    UserView now sends correct properties to VUserProfile

--- vue_js/tdd-app/tests/unit/UserView.spec.js
+++ vue_js/tdd-app/tests/unit/UserView.spec.js
@@ -1,14 +1,21 @@
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import Vuex from "vuex";
 import UserView from "@/views/UserView";
 import VUserSearchForm from "@/components/VUserSearchForm";
 import VUserProfile from "@/components/VUserProfile";
+import initialState from "@/store/state";
+import userFixture from "./fixtures/user";
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
 
 describe("UserView", () => {
+  let state;
+
   const build = () => {
     const wrapper = shallowMount(UserView, {
-      data: () => ({
-        user: {}
-      })
+      localVue,
+      store: new Vuex.Store({ state })
     });
 
     return {
@@ -17,6 +24,10 @@ describe("UserView", () => {
       userProfile: () => wrapper.find(VUserProfile)
     };
   };
+
+  beforeEach(() => {
+    state = { ...initialState };
+  });
 
   it("renders the component", () => {
     const { wrapper } = build();
@@ -31,13 +42,9 @@ describe("UserView", () => {
   });
 
   it("passes a binded user prop to user profile component", () => {
-    const { wrapper, userProfile } = build();
-    wrapper.setData({
-      user: {
-        name: "Daniel"
-      }
-    });
+    state.user = userFixture;
+    const { userProfile } = build();
 
-    expect(userProfile().vm.user).toBe(wrapper.vm.user);
+    expect(userProfile().vm.user).toBe(state.user);
   });
 });
