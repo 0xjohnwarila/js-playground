6b26129c8f81058630d1ec93e02128a40f6b2a8d
JohnNWarila <johnwarila@gmail.com>
2019-03-03 19:10:02 -0800

    added bookinstance detail

--- node/local_library/controllers/bookinstanceController.js
+++ node/local_library/controllers/bookinstanceController.js
@@ -17,8 +17,22 @@ exports.bookinstance_list = function (req, res, next) {
 };
 
 // display detail page for a specific bookinstance
-exports.bookinstance_detail = function (req, res) {
-  res.send(`NOT IMPLEMENTED: BookInstance detail: ${req.params.id}`);
+exports.bookinstance_detail = function (req, res, next) {
+  BookInstance.findById(req.params.id)
+    .populate('book')
+    .exec((err, bookinstance) => {
+      if (err) {
+        return next(err);
+      }
+      if (bookinstance == null) {
+        // no results -> throw new error for no reults
+        const err = new Error('Book copy not found');
+        err.status = 404;
+        return next(err);
+      }
+      // success -> render
+      res.render('bookinstance_detail', { title: 'Book:', bookinstance });
+    });
 };
 
 // display bookinstance create form on GET
