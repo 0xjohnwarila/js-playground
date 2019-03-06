94e58f7a4d7ab5fa65dbb99a2b6da83162f2e9c7
JohnNWarila <johnwarila@gmail.com>
2019-03-05 16:52:11 -0800

    Added create author

--- node/local_library/controllers/authorController.js
+++ node/local_library/controllers/authorController.js
@@ -1,6 +1,8 @@
 const async = require('async');
 const Author = require('../models/author');
 const Book = require('../models/book');
+const { body, validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
 
 // display list of all authors
 exports.author_list = function (req, res, next) {
@@ -47,14 +49,71 @@ exports.author_detail = function (req, res, next) {
 };
 
 // display author create form on GET
-exports.author_create_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: Author Create GET');
+exports.author_create_get = function (req, res, next) {
+  res.render('author_form', { title: 'Create Author' });
 };
 
 // handle author create on POST
-exports.author_create_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: Author create POST');
-};
+exports.author_create_post = [
+  // validate fields
+  body('first_name')
+    .isLength({ min: 1 })
+    .trim()
+    .withMessage('First name must be specified.')
+    .isAlphanumeric()
+    .withMessage('First name has non-alphanumeric characters.'),
+  body('family_name')
+    .isLength({ min: 1 })
+    .trim()
+    .withMessage('Family name must be specified.')
+    .isAlphanumeric()
+    .withMessage('Family name has non-alphanumeric characters.'),
+  body('date_of_birth', 'Invalid date of birth')
+    .optional({ checkFalsy: true })
+    .isISO8601(),
+  body('date_of_death', 'Invalid date of death')
+    .optional({ checkFalsy: true })
+    .isISO8601(),
+
+  // sanitize fields
+  sanitizeBody('first_name')
+    .trim()
+    .escape(),
+  sanitizeBody('family_name')
+    .trim()
+    .escape(),
+  sanitizeBody('date_of_birth').toDate(),
+  sanitizeBody('date_of_death').toDate(),
+
+  // process request
+  (req, res, next) => {
+    // extract validation errors
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // render errors
+      res.render('author_form', {
+        title: 'Create Author',
+        author: req.body,
+        errors: errors.array(),
+      });
+    } else {
+      // create auhtor oject with valid data
+      const author = new Author({
+        first_name: req.body.first_name,
+        family_name: req.body.family_name,
+        date_of_birth: req.body.date_of_birth,
+        date_of_death: req.body.date_of_death,
+      });
+      author.save((err) => {
+        if (err) {
+          return next(err);
+        }
+        res.redirect(author.url);
+      });
+    }
+  },
+];
 
 // display author delete form on GET
 exports.author_delete_get = function (req, res) {
